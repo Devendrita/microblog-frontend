3b1a25584c6098bc504e39ebab54c7d922d3de26
Dobleyu Be <67317944+Devendrita@users.noreply.github.com>
2025-07-01 17:19:18 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,3 +14,40 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+yarn add rox-browser
+yarn add @types/rox-browser
+
+import Rox from 'rox-browser';
+
+const flags = {
+  enableTutorial: new Rox.Flag(),
+  titleColors: new Rox.RoxString('White', ['White', 'Blue', 'Green', 'Yellow']),
+  titleSize: new Rox.RoxNumber(12, [14, 18, 24])
+};
+
+async function initPlatform() {
+  const options = {  };
+
+  // Register the flags
+  Rox.register('', flags);
+
+  // Setup connection with the feature management environment key
+  await Rox.setup('8e5f3402-a531-4b85-94cb-a6802750458c', options);
+
+  // Boolean flag example
+  if (flags.enableTutorial.isEnabled()) {
+    console.log('enableTutorial flag is true');
+    // TODO:  Put your code here that needs to be gated
+  }
+
+   // RoxString flag example
+  console.log('Title color is ' + flags.titleColors.getValue());
+
+  // RoxNumber flag example
+  console.log('Title size is ' + flags.titleSize.getValue());
+}
+
+initPlatform().then(function() {
+  console.log('Done loading CloudBees platform');
+});
